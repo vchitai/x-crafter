bf6f127c3fb6dbc601b5c17b2f5c7c45ba99ded5
vchitai <chitai.vct@gmail.com>
2021-10-24 16:23:41 +0700

    remove the parser cli

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/vchitai/x-crafter/breaker"
 	"github.com/vchitai/x-crafter/builder"
-	"github.com/vchitai/x-crafter/parser"
 )
 
 func main() {
@@ -16,7 +15,7 @@ func main() {
 		Short: fmt.Sprintf("%s is used to quickly make a go code prototype quickly become reproducible", "X-Crafter"),
 	}
 	rootCmd.AddCommand(breaker.Command())
-	rootCmd.AddCommand(parser.Command())
+	//rootCmd.AddCommand(parser.Command())
 	rootCmd.AddCommand(builder.Command())
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
